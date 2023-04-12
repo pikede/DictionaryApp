262b4ce11abbcf0c4de401416ab91acffbbaf142
Prince Ikede <princeikede@gmail.com>
2023-04-12 16:03:00 -0400

    cleans up some old code in preparation for detail view

--- app/src/main/java/com/example/dictionaryapp/dependencyinjection/DictionaryModule.kt
+++ app/src/main/java/com/example/dictionaryapp/dependencyinjection/DictionaryModule.kt
@@ -2,7 +2,7 @@ package com.example.dictionaryapp.dependencyinjection
 
 import com.example.dictionaryapp.database.UrbanDictionaryDatabase
 import com.example.dictionaryapp.repository.UrbanDictionaryRepository
-import com.example.dictionaryapp.service.DictionaryService
+import com.example.dictionaryapp.repository.service.DictionaryService
 import com.example.dictionaryapp.viewmodel.UrbanDictionaryViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
